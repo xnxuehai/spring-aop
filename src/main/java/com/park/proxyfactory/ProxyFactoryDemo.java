130b3f31a11a1d869d951f542623044b4b2eabe7
Aaron <xunxuehai521@163.com>
2020-09-29 21:22:34 +0800

    add Comment

--- src/main/java/com/park/proxyfactory/ProxyFactoryDemo.java
+++ src/main/java/com/park/proxyfactory/ProxyFactoryDemo.java
@@ -3,6 +3,7 @@ package com.park.proxyfactory;
 import com.park.proxyfactory.domain.ISubject;
 import com.park.proxyfactory.domain.impl.SubjectImpl;
 import com.park.proxyfactory.advice.MyAdvice;
+import org.aopalliance.aop.Advice;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.PointcutAdvisor;
@@ -23,16 +24,24 @@ public class ProxyFactoryDemo {
         // 创建一个代理类的工厂
         ProxyFactory proxyFactory = new ProxyFactory(new SubjectImpl());
 
-        // 切点
+        // 实例化一个切点
         NameMatchMethodPointcut nameMatchMethodPointcut = new NameMatchMethodPointcut();
+        // 设置需要匹配的方法名称
         nameMatchMethodPointcut.addMethodName("doService");
 
-        Advisor advisor = new DefaultPointcutAdvisor(nameMatchMethodPointcut, new MyAdvice());
+        // 创建自定义的通知
+        Advice myAdvice = new MyAdvice();
 
+        // 创建一个切面，并且设置切点和通知
+        Advisor advisor = new DefaultPointcutAdvisor(nameMatchMethodPointcut, myAdvice);
+
+        // 将切面设置到代理工厂中
         proxyFactory.addAdvisor(advisor);
 
+        // 通过代理工厂生成一个代理对象
         ISubject subject = (ISubject) proxyFactory.getProxy();
 
+        // 调用代理对象的方法
         subject.doService();
     }
 }
